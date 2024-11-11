685f3c372bf520ea8bd0754bacdedd33938c4c09
Sudharani Gugulothu <sudharani06112@gmail.com>
2024-11-11 17:58:50 -0500

    test

--- database/create-macosmodel-table.sql
+++ database/create-macosmodel-table.sql
@@ -3,7 +3,7 @@ CREATE TABLE macosmodel (
     id INT AUTO_INCREMENT PRIMARY KEY,
     model_name VARCHAR(255) NOT NULL,
     model_identifier VARCHAR(50) NOT NULL,
-    model_number VARCHAR(50) NOT NULL,
+    model_number VARCHAR(50) ,
     part_number VARCHAR(50) NOT NULL,
     serial_number VARCHAR(50) NOT NULL,
     darwin_os_number INT NOT NULL,
