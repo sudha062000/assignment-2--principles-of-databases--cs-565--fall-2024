92e826270af066915614285d367259ee8cc0c3ca
Sudharani Gugulothu <sudharani06112@gmail.com>
2024-11-12 00:58:48 -0500

    cleaned all the data and displayed the data

--- database/create-macos_model-table.sql
+++ database/create-macos_model-table.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS models (
+CREATE TABLE IF NOT EXISTS macos_model (
   model          VARCHAR(48)  NOT NULL,
   model_id       VARCHAR(16)  NOT NULL,
   model_number   VARCHAR(8)   DEFAULT NULL,
